4e67efa5921eeaebe2b912d78d2ea462dec2230e
root <bereketzeselassie@gmail.com>
2024-04-24 12:38:46 +0000

    fixxed

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -5,9 +5,9 @@ Main file for testing
 
 minOperations = __import__('0-minoperations').minOperations
 
-n = 4
+n = 23
 print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
 
-n = 12
+n = 22
 print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
 
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,25 +6,25 @@ def minOperations(n):
     '''minim'''
     if n <= 0:
         return 0
-    ls = []
+    ls = 0
 
     def opr(n, ls):
         '''minim'''
         if n == 1:
-            c = sum(ls)
-            return c
+            return int(ls)
         if n % 2 == 0:
-            ls.append(2)
+            ls += 2
             return opr(n/2, ls)
         elif n % 3 == 0:
-            ls.append(3)
+            ls += 3
             return opr(n/3, ls)
         elif n % 5 == 0:
-            ls.append(5)
+            ls += 5
             return opr(n/5, ls)
         elif n % 7 == 0:
-            ls.append(7)
+            ls += 7
             return opr(n/7, ls)
         else:
-            return 2
+            ls += n
+            return int(ls)
     return opr(n, ls)
