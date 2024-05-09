71375190458c5bcf44a34a7bcf9555758b797c1b
root <bereketzeselassie@gmail.com>
2024-05-09 05:40:24 +0000

    utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+''' validate utf8 '''
+
+
+def validUTF8(data):
+    ''' validate  data '''
+    try:
+        bt = bytes(data)
+        bt.decode('utf-8')
+        return True
+    except Exception:
+        return False
